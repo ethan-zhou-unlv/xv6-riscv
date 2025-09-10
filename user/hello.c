fb3ece9a3a23ad41141ffecfb92598e2dc1f3b94
Ethan Zhou <zhoue2@unlv.nevada.edu>
2025-09-10 00:32:53 -0700

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+    printf("Hello world, this is Ethan Zhou!\n");
+    printf("I am in CS 370, Fall 2025, section 1003.\n\n");
+
+    exit(0);
+}
